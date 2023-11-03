8f1d949e31254a8e1e02e0b57ae248b7b5231cf2
Foo_Fui <91517500+skjaimini9@users.noreply.github.com>
2023-11-03 13:41:37 +0530

    Update Multiclcass_zT.py

--- Multiclcass_zT.py
+++ Multiclcass_zT.py
@@ -1,30 +1,17 @@
-""" I am contineously working on code it is not in functionl condition right now on later stage i am planning to pass class veriable as class attribute. kiteext module is used for personal use not for any finencial use.
-if you want any part of code changed or have issue with it please write a mail to albeegupta121@gmail.com. just an initial stage. it is ment for public once complete. all contribution are welcome.
-i will also try to add greeks in options via  another static method. Any suggession is welcome
-Writing all over again from scratch.. Previously Many bugs
-
-Name of Contributers:
-
-1.
-2.
-3.
-
-"""
-# To Fetch Futures Symbols from Instruments Just Pass symbol i.e NSE:SYMBOL, expiry if you want expiry, instrument_segment i.e NFO-FUT
-
-
-#This Class is just to fetch Instrument Details from Zerodha
 import pandas as pd
+from kiteext import KiteExt
+import kiteapp as kt
 
 class ZerodhaDataframes:
     def __init__(self, symbol, expiry=None, instrument_segment=None, exch=None):
-        self.kite = KiteExt()
+        self.kite = kite = kt.KiteApp("Shri Kant", "GS7875", open("enc.txt", "r").read())
         self.df_instrument = self.get_instruments()
         self.lot_symbol = symbol
         self.expiry = pd.to_datetime(expiry).strftime('%Y-%m-%d') if expiry is not None else None
         self.instrument_segment = instrument_segment
         self.exch = exch
-
+        print(self.kite.quote('NSE:NIFTY 50'))
+    
     def get_instruments(self):
         print('wait 1 min...')
         instruments = "https://api.kite.trade/instruments"
@@ -57,11 +44,8 @@ class ZerodhaDataframes:
         filterd_instruments = filterd_instruments.reset_index(drop=True)
         return filterd_instruments
 
-class LiveDataframe:
-  pass #working on it...
-
 zdf_cash_bse = ZerodhaDataframes("BSE:SENSEX", exch="BSE")
-print(zdf_cash.filtered_instrument())
+print(zdf_cash_bse.filtered_instrument())
 zdf_cash_nse = ZerodhaDataframes("NSE:NIFTY BANK", exch="NSE")
 print(zdf_cash_nse.filtered_instrument())
 zdf_fut = ZerodhaDataframes("NSE:NIFTY 50", instrument_segment="NFO-FUT")
